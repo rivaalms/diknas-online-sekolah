f7e1db21d357e4ad2c13c3208f516e089be5832e
rivaalms <rvalms@icloud.com>
2023-05-06 10:52:06 +0700

    update: add footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -145,6 +145,17 @@
     <v-main id="main">
       <Nuxt/>
     </v-main>
+
+    <v-footer
+      app
+      inset
+      absolute
+      color="#ffffff"
+      class="d-flex justify-end"
+    >
+      <span class="text-caption">diknas-online © 2023</span>
+    </v-footer>
+    
   </v-app>
 </template>
 
